74ac2b9fe7a5e3635fedfdf1d0f40cb1105cbe5e
tuan0342 <anhphan0110@gmail.com>
2024-02-16 21:42:17 +0700

    bai16: Soft delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -26,7 +26,7 @@ class CourseController {
         const course = new Course(req.body);
         course
             .save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch((error) => {});
     }
 
@@ -50,7 +50,21 @@ class CourseController {
 
     // [DELETE]  /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE]  /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [PATCH]  /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
