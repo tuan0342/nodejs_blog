d0b6d797aee1ca837f3128fe2e202f0facbd49de
tuan0342 <anhphan0110@gmail.com>
2024-02-16 11:21:57 +0700

    bai11: Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,27 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET]  /
-    async index(req, res) {
-        try {
-            let data = await Course.find({});
-            if (data) {
-                res.json(data);
-            }
+    async index(req, res, next) {
+        // C1:
+        // try {
+        //     let data = await Course.find({});
+        //     if (data) {
+        //         res.json(data);
+        //     }
+        // } catch (error) {
+        //     res.status(500).json({ error: 'ERROR' });
+        // }
 
-            // res.render('home');
-        } catch (error) {
-            res.status(500).json({ error: 'ERROR' });
-        }
+        // C2:
+        Course.find({})
+            .then((course) => {
+                res.render('home', {
+                    course: multipleMongooseToObject(course),
+                });
+            })
+            .catch(next);
     }
 
     // [GET]  /search
---
+++ src/util/mongoose.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // convert object của mongoose sang object của JS
+
+    multipleMongooseToObject: function (mongooseArray) {
+        return mongooseArray.map((mongoose) => mongoose.toObject());
+    },
+
+    mongooseToObject: (mongoose) => {
+        return mongoose
+            ? mongoose.map((mongoose) => mongoose.toObject())
+            : mongoose;
+    },
+};
