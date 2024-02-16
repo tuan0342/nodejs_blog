b8d672a4906db3560c39d21a0ad5f714c6820a0c
tuan0342 <anhphan0110@gmail.com>
2024-02-16 22:13:50 +0700

    add count of trash

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,14 +5,35 @@ const { response } = require('express');
 class MeController {
     // [GET]  /me/stored/courses
     storedCourses(req, res, next) {
-        //Course.find({ deletedAt: null }) : deletedAt = null là giá trị của deletedAt trong db là null hoặc ko có deletedAt
-        Course.find()
-            .then((courses) =>
+        // Xử lý bất đồng bộ: Promise
+        Promise.all([
+            Course.find(),
+            Course.countDocuments(),
+            Course.countDocumentsWithDeleted(),
+        ])
+            .then(([courses, count, allCount]) =>
+                // kết quả trả về theo thứ tự hàm thực hiện: find() trả về courses, countDocumentsDeleted() trả về deletedCount
                 res.render('me/stored-courses', {
+                    deletedCount: allCount - count,
                     courses: multipleMongooseToObject(courses),
                 }),
             )
             .catch(next);
+
+        // --- Lấy danh sách khóa học chưa xóa mềm
+        // ------ Course.find({ deletedAt: null }) : deletedAt = null là giá trị của deletedAt trong db là null hoặc ko có deletedAt
+        // Course.find()
+        //     .then((courses) =>
+        //         res.render('me/stored-courses', {
+        //             courses: multipleMongooseToObject(courses),
+        //         }),
+        //     )
+        //     .catch(next);
+
+        // --- Đếm số lượng khóa học đã xóa mềm
+        // Course.countDocumentsDeleted()
+        //     .then((deletedCount) => {})
+        //     .catch(() => {})
     }
 
     // [GET]  /me/trash/courses
@@ -24,8 +45,6 @@ class MeController {
                 res.render('me/trash-courses', {
                     courses: multipleMongooseToObject(courses),
                 });
-
-                return;
             })
             .catch(next);
     }
